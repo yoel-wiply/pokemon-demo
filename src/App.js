6dc97923659ea2d09bbed0396bc98e820ade0614
Yoel Popovici <yoel@wiply.com>
2022-08-30 14:16:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Main from './components/Main';
+import "./components/style.css"
+function App() {
+  return (
+   <>
+    <Main/>
+   </>
+  );
+}
+
+export default App;
